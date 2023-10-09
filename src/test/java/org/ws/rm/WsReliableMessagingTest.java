3db596b867093fbfe8251d880a8f61c727365850
Peter Palaga <ppalaga@redhat.com>
2023-10-09 14:03:32 +0200

    Define the client in application.properties

--- src/test/java/org/ws/rm/WsReliableMessagingTest.java
+++ src/test/java/org/ws/rm/WsReliableMessagingTest.java
@@ -1,5 +1,6 @@
 package org.ws.rm;
 
+import io.quarkiverse.cxf.annotation.CXFClient;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.xml.ws.BindingProvider;
 import jakarta.xml.ws.Service;
@@ -12,6 +13,10 @@ import java.io.File;
 
 @QuarkusTest
 public class WsReliableMessagingTest {
+
+    @CXFClient("incorrect")
+    WsrmHelloService incorrect;
+
     @Test
     public void correct() throws Exception {
 
@@ -34,15 +39,7 @@ public class WsReliableMessagingTest {
     @Test
     public void inCorrect() throws Exception {
 
-        QName serviceName = new QName("https://quarkiverse.github.io/quarkiverse-docs/quarkus-cxf/test/ws-rm",
-                "org.ws.rm.WsrmHelloService");
-        Service service = Service
-                .create(Thread.currentThread().getContextClassLoader().getResource("wsrmIncorrect.wsdl").toURI().toURL(),
-                        serviceName);
-        WsrmHelloService proxy = service.getPort(WsrmHelloService.class, new RMStoreFeature());
-        BindingProvider bindingProvider = (BindingProvider) proxy;
-        bindingProvider.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,"http://localhost:8081/cxf/WsrmHelloServicePort");
-        final String reply = proxy.sayHello();
+        final String reply = incorrect.sayHello();
         String prefix = "WS-ReliableMessaging Hello World! seqSize: ";
         Assertions.assertThat(reply).startsWith(prefix);
         final String rawSeqSize = reply.substring(prefix.length());
